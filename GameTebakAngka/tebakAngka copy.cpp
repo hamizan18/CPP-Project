c77f206c03e58c54d0ae81bc219d7cc3ba801b76
hamizan18 <hamizanputra321@gmail.com>
2026-07-06 19:59:18 +0700

    done stage 2

--- GameTebakAngka/tebakAngka copy.cpp
+++ GameTebakAngka/tebakAngka copy.cpp
@@ -11,7 +11,7 @@ int main() {
 
     int random_numb = distr(gen);
 
-    cout << "Mulai menebak angka: ";
+    cout << "[1] Mulai menebak angka: ";
     cin >> n;
     counter +=1;
     if (n > random_numb) {
@@ -22,6 +22,19 @@ int main() {
     } else 
         cout << "Terlalu kecil!" << endl;
 
+    while (n != random_numb) {
+        cout << "[" << counter+1 <<"] Tebak Angka: ";
+        cin >> n;
+        counter+=1;
 
+        if (n > random_numb)
+        {
+            cout << "Terlalu besar!" << endl;
+        } else
+            cout << "Terlalu kecil!" << endl;
+    }
+
+    cout << "BENARRR!!\n[Jumlah tebakanmu: " << counter << "]" << endl;
+    
     return 0;
 }
