2af73df9ea449c1f92dc19408a8c1c8a601dd69c
hamizan18 <hamizanputra321@gmail.com>
2026-07-09 23:04:17 +0700

    1/2

--- SistemManajemenKontakTeman/kontakTeman.cpp
+++ SistemManajemenKontakTeman/kontakTeman.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-
-using namespace std;
-
-#include <iostream>
+#include <vector>
 
 using namespace std;
 struct Kontak {
@@ -13,7 +10,34 @@ struct Kontak {
 
 int main() {
 
+    vector<Kontak> daftarKontak;
+    string namaUser, emailUser;
+    int option = 0, i, noHpUser;
+
     
+    while (option != 4) {
+        cout << "Pilih opsi:\n1. Tambah Kontak\n2. Lihat Seluruh Daftar Kontak\n3. Cari Kontak Berdasarkan Nama\n4. Keluar" << endl;
+        cin >> option;
+        if (option == 1) {
+            cout << "Masukkan Nama: " << endl;
+            cin >> namaUser;
+            cout << "Masukkan Nomor HP: " << endl;
+            cin >> noHpUser;
+            cout << "Masukkan Email: " << endl;
+            cin >> emailUser;
+
+            daftarKontak.push_back(daftarKontak.nama, noHpUser, emailUser);
+        } else if (option == 2) {
+            for(i;i < daftarKontak.size();i++){
+                cout << i+1 << ". " << "Nama: " << daftarKontak[i].nama << endl;
+                cout << i+1 << ". " << "Nama: " << daftarKontak[i].noHp << endl;
+                cout << i+1 << ". " << "Nama: " << daftarKontak[i].email << endl;
+            }
+        } else if (option == 3) {
+
+        } else
+            cout << "Inputan Tidak Valid!";
+    }
 
     return 0;
 }
