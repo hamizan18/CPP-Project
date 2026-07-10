c5b3e703f6d74493af1e436098eca2c47db9cafa
hamizan18 <hamizanputra321@gmail.com>
2026-07-10 23:56:22 +0700

    tambah loop menu

--- 3. SistemKasirTokoMini/sistemKasirTokoMini.cpp
+++ 3. SistemKasirTokoMini/sistemKasirTokoMini.cpp
@@ -3,20 +3,24 @@
 
 using namespace std;
 int menuBarang() {
-    int choice, question;
+    int choice, button = 0;
+    char question = 'y';
 
-    cout << "Wanna buy more? (y/n): ";
-    cin >> question;
-
-    while ()
-    cout << "\t[READY ITEM]"<< endl;
-    cout << "1. Gamepad\t| Rp120.000" << endl;
-    cout << "2. Deskmat\t| Rp70.000" << endl;
-    cout << "3. Mouse\t| Rp160.000" << endl;
-    cout << "4. Keyboard\t| Rp350.000" << endl; 
-    cout << "Pilih barang yang ingin anda beli: ";
-    cin >> choice;
-
+    
+    while (question != 'n') {
+        if (button == 1) {
+            cout << "Wanna buy more? (y/n): ";
+            cin >> question;
+        }
+        cout << "\t[READY ITEM]"<< endl;
+        cout << "1. Gamepad\t| Rp120.000" << endl;
+        cout << "2. Deskmat\t| Rp70.000" << endl;
+        cout << "3. Mouse\t| Rp160.000" << endl;
+        cout << "4. Keyboard\t| Rp350.000" << endl; 
+        cout << "Pilih barang yang ingin anda beli: ";
+        cin >> choice;
+        button = 1;
+    }
     return choice;
 }
 
