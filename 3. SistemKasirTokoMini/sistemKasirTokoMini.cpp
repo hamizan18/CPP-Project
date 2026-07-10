2dc1b1f4734fac2a836852317356d510f32d9113
hamizan18 <hamizanputra321@gmail.com>
2026-07-10 20:18:17 +0700

    almost done

--- 3. SistemKasirTokoMini/sistemKasirTokoMini.cpp
+++ 3. SistemKasirTokoMini/sistemKasirTokoMini.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-int main() {
+int menuBarang() {
+    int choice, question;
 
-    
+    cout << "Wanna buy more? (y/n): ";
+    cin >> question;
+
+    while ()
+    cout << "\t[READY ITEM]"<< endl;
+    cout << "1. Gamepad\t| Rp120.000" << endl;
+    cout << "2. Deskmat\t| Rp70.000" << endl;
+    cout << "3. Mouse\t| Rp160.000" << endl;
+    cout << "4. Keyboard\t| Rp350.000" << endl; 
+    cout << "Pilih barang yang ingin anda beli: ";
+    cin >> choice;
+
+    return choice;
+}
+
+int main() {
+    int choice;
+
+    choice = menuBarang();
+
+
 
     return 0;
 }
